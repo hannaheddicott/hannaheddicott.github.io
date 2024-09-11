77069162f9b840f01c0e425d9645a86cb7dc02d5
hannaheddicott <hannaheddicott@hotmail.com>
2024-09-11 08:39:24 +0200

    core changes to html and css

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const images = document.querySelectorAll("img");
 
     for(const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
+        fetch("https://zedicot.com/wp-content/uploads/2024/06/zedicot-merch-013.jpg")
         .then(response => response.json())
         .then(data => {
             image.src = data.message
